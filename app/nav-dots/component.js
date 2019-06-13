00fd1b101ff44603af8470a3c09f4cc23a099aa4
Marco Umpiérrez <marcoumpierrez@gmail.com>
2019-06-13 20:32:29 +0200

    Added ember-cli-sass Used pod structure for components Used ember-component-css to manage the styles per component Added nav-dots component for UX identifying possible gestures Added calendar-grid component

---
+++ app/nav-dots/component.js
@@ -0,0 +1,22 @@
+import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
+
+export default class NavDotsComponent extends Component {
+  @service router
+
+  @computed('router.currentRouteName')
+  get isCalendarRoute() {
+    return this.router.currentRouteName.includes('calendar') ? 'light-on' : 'light-off';
+  }
+
+  @computed('router.currentRouteName')
+  get isIndexRoute() {
+    return this.router.currentRouteName === 'index' ? 'light-on' : 'light-off';
+  }
+
+  @computed('router.currentRouteName')
+  get isMeasuresRoute() {
+    return this.router.currentRouteName.includes('measures') ? 'light-on' : 'light-off';
+  }
+}
