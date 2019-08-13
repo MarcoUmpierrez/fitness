87fe0ad1cccdd0e75538b9916d02292f76d65fd7
Marco Umpiérrez <marcoumpierrez@gmail.com>
2019-08-14 00:23:15 +0200

    [BUGFIX] switched input width to percentage value [ENHANCEMENT] Added date to measure form

--- app/calendar/measure-new/controller.ts
+++ app/calendar/measure-new/controller.ts
@@ -1,25 +1,15 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import Measure from 'fitness/models/measure';
-import { inject as service } from '@ember/service';
-import SelectedDateService from 'fitness/services/selected-date';
 
 export default class MeasuresNewController extends Controller {
-  @service
-  selectedDate?: SelectedDateService;
-
   @action
   save() {
-    if (!this.selectedDate) {
-      return;
-    }
-
     let measure: Measure = this.model;
     if (!measure.muscle && !measure.water && !measure.weight && !measure.boneDensity && !measure.fat) {
       return;
     }
 
-    this.model.set('date', this.selectedDate.date);
     this.model.save();
 
     this.transitionToRoute('calendar');
--- app/calendar/measure-new/route.ts
+++ app/calendar/measure-new/route.ts
@@ -1,7 +1,20 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import SelectedDateService from 'fitness/services/selected-date';
 
 export default class MeasureNewRoute extends Route {
+  @service
+  selectedDate?: SelectedDateService;
+
   model() {
     return this.store.createRecord('measure');
+  }
+
+  afterModel(model, transition) {
+    if (this.selectedDate && this.selectedDate.date) {
+      model.set('date', this.selectedDate.date);
+    } else {
+      model.set('date', new Date());
+    }
   }
 }
