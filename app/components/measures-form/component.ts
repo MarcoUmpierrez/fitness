7258ec216249ae97efee04c95a9ec00eaada8932
Marco Umpiérrez <marcoumpierrez@gmail.com>
2019-08-17 01:07:24 +0200

    Hide navbar when soft keyboard is shown

--- app/components/measures-form/component.ts
+++ app/components/measures-form/component.ts
@@ -1,3 +1,25 @@
 import Component from '@ember/component';
 
-export default class MeasuresFormComponent extends Component {}
+export default class MeasuresFormComponent extends Component {
+  didRender() {
+    super.didRender();
+    let inputs = Array.from(document.getElementsByTagName('input'));
+    inputs.forEach(input => input.addEventListener('focus', this.hideNavBar));
+    inputs.forEach(input => input.addEventListener('blur', this.showNavBar));
+  }
+
+  willDestroy() {
+    let inputs = Array.from(document.getElementsByTagName('input'));
+    inputs.forEach(input => input.removeEventListener('focus', this.hideNavBar));
+    inputs.forEach(input => input.removeEventListener('blur', this.showNavBar));
+    super.willDestroy();
+  }
+
+  hideNavBar() {
+    document.getElementsByClassName('footer')[0].style = 'display:none';
+  }
+
+  showNavBar() {
+    document.getElementsByClassName('footer')[0].style = 'display:inherit';
+  }
+}
