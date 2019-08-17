5a03ec531711c3e7777987c7ee489276dc42e8f7
Marco Umpiérrez <marcoumpierrez@gmail.com>
2019-08-17 14:48:33 +0200

    Control navbar state using media queries

--- app/components/measures-form/component.ts
+++ app/components/measures-form/component.ts
@@ -1,31 +1,4 @@
 import Component from '@ember/component';
 
 export default class MeasuresFormComponent extends Component {
-  didRender() {
-    super.didRender();
-    let inputs = Array.from(document.getElementsByTagName('input'));
-    inputs.forEach(input => input.addEventListener('focus', this.hideNavBar));
-    inputs.forEach(input => input.addEventListener('blur', this.showNavBar));
-  }
-
-  willDestroy() {
-    let inputs = Array.from(document.getElementsByTagName('input'));
-    inputs.forEach(input => input.removeEventListener('focus', this.hideNavBar));
-    inputs.forEach(input => input.removeEventListener('blur', this.showNavBar));
-    super.willDestroy();
-  }
-
-  hideNavBar() {
-    let footers = document.getElementsByTagName('footer') as HTMLCollectionOf<HTMLElement>;
-    if (footers && footers.length > 0) {
-      footers[0].style.display = 'none';
-    }
-  }
-
-  showNavBar() {
-    let footers = document.getElementsByTagName('footer') as HTMLCollectionOf<HTMLElement>;
-    if (footers && footers.length > 0) {
-      footers[0].style.display = 'inherit';
-    }
-  }
 }
