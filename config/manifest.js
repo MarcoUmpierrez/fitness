e900460fee5008bfe5684aca8479bb75d9421704
Marco Umpiérrez <marcoumpierrez@gmail.com>
2019-08-25 19:57:24 +0200

    Sigh...

--- config/manifest.js
+++ config/manifest.js
@@ -18,25 +18,25 @@ module.exports = function (/* environment, appConfig */) {
     theme_color: "#E53E3E",
     icons: [
       {
-        src: "/assets/logo/logo-180x180.png",
+        src: "/assets/icons/logo-180x180.png",
         sizes: "180x180"
       },
       {
-        src: "/assets/logo/logo-512x512.png",
+        src: "/assets/icons/logo-512x512.png",
         sizes: "512x512"
       },
       {
-        src: '/assets/logo/logo-280x280.png',
+        src: '/assets/icons/logo-280x280.png',
         sizes: '280x280',
         targets: ['apple'],
       },
       {
-        src: '/assets/logo/logo-32x32.png',
+        src: '/assets/icons/logo-32x32.png',
         sizes: '32x32',
         targets: ['favicon'],
       },
       {
-        src: '/assets/logo/logo-150x150.png',
+        src: '/assets/icons/logo-150x150.png',
         element: 'square150x150logo',
         targets: ['ms'],
       }
